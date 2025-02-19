f7c18c7d4440393770b661eb5f281b425442a100
Manish Dait <daitmanish88@gmail.com>
2025-02-19 20:31:53 +0530

    remove unwanted cmd from bash

--- dockerize.sh
+++ dockerize.sh
@@ -2,14 +2,13 @@
 set -e
 
 echo -e "🐳 Dockerizing API \n"
-cd latte-api > /dev/null
+cd latte-api
 docker build -t api . > /dev/null
 
 cd ..
 
 echo -e "\n🐳 Dockerizing CLIENT \n"
 cd latte-client
-npm run build > /dev/null
 docker build -t client . > /dev/null
 rm -rf dist
 
